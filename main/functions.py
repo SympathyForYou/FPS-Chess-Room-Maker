9288419db5d7af65188cfbbc7874bad9a85d7964
SympathyForYou <vornicuemanuelcristi@gmail.com>
2025-03-19 22:50:32 -0500

    New flow system

--- main/functions.py
+++ main/functions.py
@@ -1,10 +1,10 @@
-from config import BUTTON_POSITIONS # Used to connect dictionary to function
 from typing import Tuple, Callable, Any, Dict  # Used to create a tuple type hint
 import pyautogui  # Used to move the cursor
 import argparse  # Used to add --yes / --no argument for game-mode selection
 import time
 
-def is_on_screen(image, confidence) -> bool | None:
+
+def is_on_screen(image, confidence=0.6) -> bool | None:
     """
     It takes a screenshot of the entire screen each couple moments and tries to find the given image
     :param image: The path of the image you want to find
@@ -19,7 +19,7 @@ def is_on_screen(image, confidence) -> bool | None:
         return False  # Returns false (It didn't find)
 
 
-def find_on_screen(image, confidence=0.7) ->  None:
+def find_on_screen(image, confidence=0.7) -> None:
     """
     Based on is_on_screen() function. It loops until it finds the desired image
     :param image: "The path of the image you want to find"
@@ -28,60 +28,74 @@ def find_on_screen(image, confidence=0.7) ->  None:
     """
     while True:
         time.sleep(1)
-        if not is_on_screen(image, confidence):
+        if not is_on_screen(image, confidence=confidence):
             pass
         else:
             break
 
 
-def find_and_click(image, confidence=0.7, times=1) -> None:
-    while True:
+def find_and_click(image, confidence=0.7, times=1) -> bool:
+    """
+    Finds an image and clicks in the middle of it.
+    :param image: "The path of the image you want to find"
+    :param confidence: "How much you want your image on screen to resemble your own image to be considered a match (0.1 - 1)"
+    :param times: How many times you want to click.
+    :return: A bool
+    """
+    tries = 0
+    while tries < 20:
         try:
             x, y = pyautogui.locateCenterOnScreen(image, confidence=confidence)
             for i in range(times):
                 pyautogui.moveTo(x, y)
                 pyautogui.click()
-            break
+            return True
         except pyautogui.ImageNotFoundException:
             pass
+        tries += 1
+        # print(tries)
+    return False
 
 
 def clear_and_type(name) -> None:
     """
-      Clears a text box and types new text.
-
-      :param name: Text to type.
-      :return: None
-      """
-    pyautogui.keyDown('ctrl')
-    pyautogui.press('a')
-    pyautogui.keyUp('ctrl')
-    pyautogui.press('backspace')
+    Clears a text box and types new text.
+    :param name: Text to type.
+    :return: None
+    """
+    pyautogui.keyDown("ctrl")
+    pyautogui.press("a")
+    pyautogui.keyUp("ctrl")
+    pyautogui.press("backspace")
     pyautogui.typewrite(name)
 
 
 def parse_arguments() -> argparse.Namespace:
     """
     Parses command-line arguments for Shiny Chess mode.
-
-    Returns:
-        Parsed arguments with --yes or --no flags.
+    :returns: Parsed arguments with --yes or --no flags.
     """
     parser = argparse.ArgumentParser(description="Chess game settings")
-    parser.add_argument('-y', '--yes', action='store_true', help="Automatically set Shiny Chess to yes")
-    parser.add_argument('-n', '--no', action='store_true', help="Automatically set Shiny Chess to no")
+    parser.add_argument(
+        "-y", "--yes", action="store_true", help="Automatically set Shiny Chess to yes"
+    )
+    parser.add_argument(
+        "-n", "--no", action="store_true", help="Automatically set Shiny Chess to no"
+    )
     return parser.parse_args()
 
 
 def measure_time(func) -> Callable[[tuple[Any, ...], dict[str, Any]], None]:
     """
     Decorator to measure a function's execution time.
-
     :param func: Function to measure.
     """
     def wrapper(*args, **kwargs) -> None:
         start = time.time()
         func(*args, **kwargs)
         end = time.time()
-        print(f"Time it took to execute {str(func).split(' ')[1]}: {(end - start):.2f} seconds")
+        print(
+            f"Time it took to execute {str(func).split(' ')[1]}: {(end - start):.2f} seconds"
+        )
+
     return wrapper
--- main/main.py
+++ main/main.py
@@ -1,76 +1,200 @@
-import pyautogui
-
-from functions import is_on_screen, find_and_click, clear_and_type, parse_arguments, measure_time  # Explicit imports
-from config import IMAGE_PATHS, DESCRIPTION, NORMAL_ROOM_NAME, SHINY_ROOM_NAME # Config
+from config import IMAGE_PATHS, DESCRIPTION, NORMAL_ROOM_NAME, SHINY_ROOM_NAME  # Config
+import pyautogui  # Used for mouse / keyboard inputs
 import logging  # Used to log print messages
+import time  # Used to make the program wait
+from functions import (
+    is_on_screen,
+    find_and_click,
+    clear_and_type,
+    parse_arguments,
+    measure_time,
+)
+
+
+def find_host_button() -> None:
+    """
+    Clicks on the host button that leads to the create room menu.
+    :return: None
+    """
+    # Click on the Host Button
+    if find_and_click(IMAGE_PATHS["host"]):
+        logging.info("Clicked Host Button")
+    else:
+        main()
+
+
+def set_room_name(shiny_mode) -> None:
+    """
+    Sets room's name based on the shiny flag.
+    :param shiny_mode: See get_shiny_mode_from_input function.
+    :return: None
+    """
+    # Set room name
+    room_name = SHINY_ROOM_NAME if shiny_mode else NORMAL_ROOM_NAME
+    if find_and_click(IMAGE_PATHS["room_name"]):
+        clear_and_type(room_name)
+        logging.info(f"Set room name to: {room_name}")
+    else:
+        main()
+
+
+def set_description() -> None:
+    """
+    Sets room's description.
+    :return: None
+    """
+    if find_and_click(IMAGE_PATHS["description"]):
+        clear_and_type(DESCRIPTION)
+        logging.info(f"Set description to: {DESCRIPTION}")
+    else:
+        main()
+
+
+def set_game_mode(shiny_mode) -> None:
+    """
+    Clicks on the shiny game-mode checkbox.
+    :param shiny_mode: See get_shiny_mode_from_input function.
+    :return: None
+    """
+    # Set game-mode
+    if shiny_mode:
+        if find_and_click(IMAGE_PATHS["shiny"], confidence=0.95):
+            logging.info("Activated Shiny Game Mode")
+        else:
+            main()
+
+
+def create_room() -> None:
+    """
+    Clicks on the launch room button.
+    :return: None
+    """
+    # Creates a room
+    if find_and_click(IMAGE_PATHS["create_room"]):
+        logging.info("Lobby Launched")
+    else:
+        main()
+
+
+def close_invites() -> None:
+    """
+    Closes the friends invite window.
+    :return: None
+    """
+    # Close invites
+    if find_and_click(IMAGE_PATHS["close"]):
+        logging.info("Closed invites window")
+    else:
+        main()
+
+
+def game_ended(shiny_mode) -> None:
+    """
+    Whenever someone wins / player leaves it quits to main menu.
+    :param shiny_mode: See get_shiny_mode_from_input function.
+    :return: None
+    """
+    # Set description
+    pyautogui.hotkey("esc")
+    if find_and_click(IMAGE_PATHS["menu"], times=2):
+        create_lobby(shiny_mode)
+        logging.info("Restart game.")
+    else:
+        main()
+
+
+def define_stage() -> int:
+    """
+    Takes a look at your screen using is_on_screen(image) and figures out what stage you on.
+    :return: An integer corresponding to the current stage your game is that gets plugged into a list.
+    """
+    while True:
+        if is_on_screen(IMAGE_PATHS["host"]):
+            return 0
+        elif is_on_screen(IMAGE_PATHS["room_name"]):
+            return 1
+        elif is_on_screen(IMAGE_PATHS["description"]):
+            return 2
+        elif is_on_screen(IMAGE_PATHS["shiny"], 0.95):
+            return 3
+        elif is_on_screen(IMAGE_PATHS["create_room"]):
+            return 4
+        elif is_on_screen(IMAGE_PATHS["close"]):
+            return 5
+        elif is_on_screen(IMAGE_PATHS["left"]):
+            logging.info("Player Left")
+            return 7
+        elif is_on_screen(IMAGE_PATHS["white"]):
+            logging.info("White Won")
+            return 7
+        elif is_on_screen(IMAGE_PATHS["black"]):
+            logging.info("Black Won")
+            return 7
+        elif is_on_screen(IMAGE_PATHS["failure"]):
+            logging.info("Connection Failed")
+            return 8
+        elif is_on_screen(IMAGE_PATHS["host_left"]):
+            return 8
+        # print("full rotation")
 
 
 # The main flow of the program is here
 @measure_time
 def create_lobby(shiny_mode: bool) -> None:
     """
-    Creates a game lobby based on the shiny_mode flag.
-
+    Creates a game lobby based on the shiny_mode flag and current_stage function.
     :param shiny_mode: If True, sets up a shiny chess game. Otherwise, sets up a normal game.
     :return: None
     """
-    # Find main menu
-    while True:
-        if is_on_screen(IMAGE_PATHS['main'], 0.5):
-            break
-        elif is_on_screen(IMAGE_PATHS['left'], 0.7):
-            logging.info("Player left")
-            pyautogui.hotkey('esc')
-            find_and_click(IMAGE_PATHS['menu'], confidence=0.7, times=2)
-            create_lobby(shiny_mode)
 
-    # Click on the Host Button (Three times to un-focus from konsole)
-    find_and_click(IMAGE_PATHS['host'])
-    logging.info("Clicked Host Button")
+    # Functions to loop through
+    functions = [
+        find_host_button,
+        set_room_name,
+        set_description,
+        set_game_mode,
+        create_room,
+        close_invites,
+    ]
+    current_stage = define_stage()
 
+    # print(current_stage) -- Debug
+    # Looping sequence
+    if current_stage < 7:
+        for func in functions[current_stage:]:
+            # print(functions[current_stage].__name__) -- Debug
+            func_name = func.__name__
 
-    # Set room name based on game-mode
-    room_name = SHINY_ROOM_NAME if shiny_mode else NORMAL_ROOM_NAME
-    find_and_click(IMAGE_PATHS['room_name'])
-    clear_and_type(room_name)
-    logging.info(f"Set room name to: {room_name}")
+            if func_name not in ["set_room_name", "set_game_mode", "game_ended"]:
+                time.sleep(0.2)
+                func()
+            else:
+                func(shiny_mode)
+    # If host left or failure
+    elif current_stage == 8:
+        print("press esc")
+        pyautogui.press("esc")
+    # Game Over
+    else:
+        game_ended(shiny_mode)
 
-
-    # Set description
-    find_and_click(IMAGE_PATHS['description'])
-    clear_and_type(DESCRIPTION)
-    logging.info(f"Set description to: {DESCRIPTION}")
-
-    # Set game-mode
-    if shiny_mode:
-        find_and_click(IMAGE_PATHS['shiny'], confidence=0.95)
-        logging.info("Activated Shiny Game Mode")
-
-    # Creates a room
-    find_and_click(IMAGE_PATHS['create_room'])
-    logging.info("Lobby Launched")
-
-    # Close invites
-    logging.info("Invites Found")
-    find_and_click(IMAGE_PATHS['close'])
-    logging.info("Closed invites window")
 
 def get_shiny_mode_from_input() -> bool:
     """
     Prompts the user interactively to determine whether to enable shiny mode.
-
     :return: True if shiny mode is enabled; False otherwise.
     """
     while True:
         user_input = input("Shiny Chess? (y/n): ").strip().lower()
-        if user_input in {'y', 'n'}:
-            return user_input == 'y'
+        if user_input in {"y", "n"}:
+            return user_input == "y"
         print("Invalid input. Please enter 'y' or 'n'.")
 
 
 def main() -> None:
     """
     Main entry point for the program. Parses arguments and creates a lobby.
+    :return: None
     """
     args = parse_arguments()
 
@@ -86,6 +210,7 @@ def main() -> None:
     while True:
         create_lobby(shiny_mode)
 
+
 # Main call
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
