e17b7d0d93863f8f0cab8697d16b0abf50b79bde
SympathyForYou <vornicuemanuelcristi@gmail.com>
2025-04-16 22:54:10 -0500

    class update

--- main/functions.py
+++ main/functions.py
@@ -1,75 +1,72 @@
-from config import IMAGE_PATHS, SHINY_ROOM_NAME, NORMAL_ROOM_NAME, DESCRIPTION
-import pyautogui  # (Third-Party) Used for automation (clicking, typing)
-import logging    # (Built-in) Used to print classy messages
-import time       # (Built-in) Used for the measure_time decorator
-import sys        # (Built-in) Used to add --yes / --no cmd arguments
+from logging import info  # (Built-in) Used to print classy messages
+from time import time     # (Built-in) Used for the measure_time decorator
+from sys import argv      # (Built-in) Used to add --yes / --no cmd arguments
+import pyautogui          # (Third-Party) Used for automation (clicking, typing)
 
 
 class ImageRecognition:  # This is more of a folder than a class
-    """
-    This class contains functions related to finding images on the screen.
-    """
-
-    def is_on_screen(image: str, confidence: float=0.6) -> bool:
         """
-        Takes a screenshot of your entire screen each couple moments and attempts to find the given image.
-        :param image: The path of the image you want to find.
-        :param confidence: How much you want you want your image to match before it's a result -- From 0.1 to 1 -- I recommand between 0.6 and 0.8.
-        :return: bool
+        This class contains functions related to finding images on the screen.
         """
-        try:  # Attempts to find the image if it can't, it throws an error
-            if pyautogui.locateOnScreen(image, confidence=confidence):
-                return True
-        except pyautogui.ImageNotFoundException:
-            return False  # Returns false (It didn't find)
 
-
-    def find_and_click(image: str, confidence: float=0.7, clicks: int=1) -> bool:
-        """
-        Attempts to find your image and clicks in the middle of it.
-        :param image: The path of the image you want to find.
-        :param confidence: How much you want you want your image to match before it's a result -- From 0.1 to 1 -- I recommand between 0.6 and 0.8.
-        :param clicks: How many times you want to click.
-        :return: bool
-        """
-        attempts: int = 0
-
-        while attempts < 35:  # KEEP AT 35 or it skips next stage after find_host_button
-            try:
-                # Get coordinates
-                x, y = pyautogui.locateCenterOnScreen(image, confidence=confidence)
-                # Click on the image
-                for i in range(clicks):
-                    pyautogui.moveTo(x, y)
-                    pyautogui.click()
-                return True
+        @staticmethod
+        def is_on_screen(image: str, confidence: float=0.6) -> bool:
+            """
+            Takes a screenshot of your entire screen each couple moments and attempts to find the given image.
+            :param image: The path of the image you want to find.
+            :param confidence: How much you want you want your image to match before it's a result -- From 0.1 to 1 -- I recommand between 0.6 and 0.8.
+            """
+            try:  # Attempts to find the image if it can't, it throws an error
+                if pyautogui.locateOnScreen(image, confidence=confidence):
+                    return True
             except pyautogui.ImageNotFoundException:
-                attempts += 1
-        
-        return False
+                return False  # Returns false (It didn't find)
+
+
+        @staticmethod
+        def find_and_click(image: str, confidence: float=0.7, clicks: int=1) -> bool:
+            """
+            Attempts to find your image and clicks in the middle of it.
+            :param image: The path of the image you want to find.
+            :param confidence: How much you want you want your image to match before it's a result -- From 0.1 to 1 -- I recommand between 0.6 and 0.8.
+            :param clicks: How many times you want to click.
+            """
+            attempts: int = 0
+
+            while attempts < 35:  # KEEP AT 35 or it skips next stage after find_host_button (set_room_name)
+                try:              # This has to due to the fact that a bad looping cycle might detect a later stage earlier then it should
+                    # Get coordinates
+                    x, y = pyautogui.locateCenterOnScreen(image, confidence=confidence)
+                    # Click on the image
+                    for i in range(clicks):
+                        pyautogui.moveTo(x, y)
+                        pyautogui.click()
+                    return True
+                except pyautogui.ImageNotFoundException:
+                    attempts += 1
+            
+            return False
 
 
 class Backend:  # This is more of a folder than a class
     """
     Miscellaneous functions related to:
-    - parsing arguments, 
     - measuring function time,
-    - getting user input 
-    - and typing.
+    - getting user input,
+    - and typing,
     """
 
+    @staticmethod
     def measure_time(func):
         """
         Decorator to measure a function's execution time.
-        :param func: Function to measure.
-        :return: Wrapper function that prints the execution time.
         """
         def wrapper(*args, **kwargs) -> None:
             # Start time
-            start: float = time.time()
+            start: float = time()
             result: any = func(*args, **kwargs)
             # End time
-            end: float = time.time()
+            end: float = time()
             # Printing the result
             print("-" * 20)
             print(f"The time your game took: {(end - start):.2f} seconds to complete")
@@ -78,31 +75,31 @@ class Backend:  # This is more of a folder than a class
         return wrapper
 
 
+    @staticmethod
     def user_input() -> bool:
         """
         Takes user input via command line arguments or interactively.
-        :return: bool
         """
         # Determine shiny_mode_flag based on the first command-line argument
-        USER_ARGUMENT: list = sys.argv if len(sys.argv) > 0 else ""
+        USER_ARGUMENT: list = argv if len(argv) > 0 else ""
         if len(USER_ARGUMENT) > 1 and USER_ARGUMENT[1] in {'--yes', '-y', '--no', '-n'}:
             return USER_ARGUMENT[1] in {'--yes', '-y'}
         
         # Determine shiny_mode_flag based on the interactive prompt
         while True:
-            logging.info("Do you want to play Shiny Chess? (y/n)")
+            info("Do you want to play Shiny Chess? (y/n)")
             USER_INPUT: str = input(">>> ").strip().lower()
             if USER_INPUT in {"y", "n"}:
                 return USER_INPUT == "y"
             
-            logging.info("Invalid input. Please enter 'y' or 'n'.")
+            info("Invalid input. Please enter 'y' or 'n'.")
 
 
+    @staticmethod
     def clear_and_type(sentence: str) -> None:
         """
         Takes control of your keyboard and clears a text box and types new text.
         :param sentence: Text to type.
-        :return: None
         """
         # Select all text
         pyautogui.keyDown("ctrl")
@@ -114,107 +111,70 @@ class Backend:  # This is more of a folder than a class
         pyautogui.typewrite(sentence)
 
 
-class Stage:  # This is more of a folder than a class
-    """
-    This class contains all stage functions the program must run to create a chess room.
-    """
+class RoomSetup:  # An actual class
+    @staticmethod
+    def _detect_stage() -> None:
+        from main import detect_stage
+        detect_stage()
+
+
+    @staticmethod
+    def _click_and_log(img_path: str, log_message: str, confidence: float=0.7, clicks: int=1) -> None:
+        if ImageRecognition.find_and_click(img_path, confidence=confidence, clicks=clicks):
+            info(log_message)
+
+        RoomSetup._detect_stage()
     
-    def find_host_button(game_mode_flag: bool) -> None:
-        """ 
-        Clicks on the host button in main menu that leads to the create room menu.
-        :param game_mode_flag: If True, sets up a shiny chess game. Otherwise, sets up a normal game.
-        :return: None
-        """
-        from main import detect_stage
-        # Click on the Host Button
-        if ImageRecognition.find_and_click(IMAGE_PATHS["host_text_in_main_menu"]):
-            logging.info("Clicked Host Button")
-        
-        detect_stage()
+
+    @staticmethod
+    def find_host_button(img_path, game_mode_flag: bool) -> None:
+        """ Clicks on the host button in main menu that leads to the create room menu."""
+
+        RoomSetup._click_and_log(img_path, log_message="Clicked Host Button")
 
 
-    def set_room_name(game_mode_flag: bool) -> None:
-        """
-        Sets room's name based on the game_mode.
-        :param game_mode_flag: If True, sets up a shiny chess game. Otherwise, sets up a normal game.
-        :return: None
-        """
-        from main import detect_stage
-        # Set room name
-        room_name = SHINY_ROOM_NAME if game_mode_flag else NORMAL_ROOM_NAME
-        if ImageRecognition.find_and_click(IMAGE_PATHS["room_name_text_box"]):
-            logging.info(f"Set room name to: {room_name}")
-            Backend.clear_and_type(room_name)
-        
-        detect_stage()
+    @staticmethod
+    def set_room_name(img_path, game_mode_flag: bool) -> None:
+        """Sets room's name based on the game_mode."""
+        from main import NORMAL_ROOM_NAME, SHINY_ROOM_NAME
+        ROOM_NAME = SHINY_ROOM_NAME if game_mode_flag else NORMAL_ROOM_NAME
+        if ImageRecognition.find_and_click(img_path):
+            info(f"Set room namet to: {ROOM_NAME}")
+            Backend.clear_and_type(ROOM_NAME)
 
 
-    def set_description(game_mode_flag: bool) -> None:
-        """
-        Sets room's description.
-        :param game_mode_flag: If True, sets up a shiny chess game. Otherwise, sets up a normal game.
-        :return: None
-        """
-        from main import detect_stage
-        if ImageRecognition.find_and_click(IMAGE_PATHS["description_text_box"]):
+    @staticmethod
+    def set_description(img_path: str, game_mode_flag: bool) -> None:
+        """Sets room's description."""
+        from main import DESCRIPTION
+        if ImageRecognition.find_and_click(img_path):
+            info("Set description to: {DESCRIPTION}")
             Backend.clear_and_type(DESCRIPTION)
-            logging.info(f"Set description to: {DESCRIPTION}")
-        
-        detect_stage()
+    
 
-
-    def set_game_mode(game_mode_flag: bool) -> None:
-        """
-        Clicks on the shiny game-mode checkbox.
-        :param game_mode_flag: If True, sets up a shiny chess game. Otherwise, sets up a normal game.
-        :return: None
-        """
-        from main import detect_stage
-        # Set game-mode
+    @staticmethod
+    def set_game_mode(img_path: str, game_mode_flag: bool) -> None:
+        """Clicks on the shiny game-mode checkbox."""
         if game_mode_flag:
-            ImageRecognition.find_and_click(IMAGE_PATHS["shiny_game_mode_checkbox"], confidence=0.95)
-            logging.info("Activated Shiny Game Mode")
-            
-        detect_stage()
-
-
-    def create_room(game_mode_flag: bool) -> None:
-        """
-        Clicks on the launch room button.
-        :param game_mode_flag: If True, sets up a shiny chess game. Otherwise, sets up a normal game.
-        :return: None
-        """
-        from main import detect_stage
-        # Creates a room
-        if ImageRecognition.find_and_click(IMAGE_PATHS["create_room_button"]):
-            logging.info("Lobby Launched")
+            RoomSetup._click_and_log(img_path, log_message="Activated Shiny Game Mode", confidence=0.95,)
         
-        detect_stage()
+        RoomSetup._detect_stage()
 
 
-    def close_invites(game_mode_flag: bool) -> None:
-        """
-        Clicks on the close friends invite window.
-        :param game_mode_flag: If True, sets up a shiny chess game. Otherwise, sets up a normal game.
-        :return: None
-        """
-        from main import detect_stage
-        # Close invites
-        if ImageRecognition.find_and_click(IMAGE_PATHS["close_invites_button"]):
-            logging.info("Closed invites window")
-        
-        detect_stage()
+    @staticmethod
+    def create_room(img_path: str, game_mode_flag: bool) -> None:
+        """Clicks on the launch room button."""
+        RoomSetup._click_and_log(img_path, log_message="Lobby Launched")
 
 
-    def game_ended(game_mode_flag: bool) -> None:
-        """
-        Whenever someone wins / player leaves it quits to main menu.
-        :param game_mode_flag: If True, sets up a shiny chess game. Otherwise, sets up a normal game.
-        :return: None
-        """
-        from main import detect_stage
+    @staticmethod
+    def close_invites(img_path: str, game_mode_flag: bool) -> None:
+        """Clicks on the close friends invite window."""
+        RoomSetup._click_and_log(img_path, log_message="Closed invites window")
+    
+
+    @staticmethod
+    def game_ended(img_path: str, game_mode_flag: bool) -> None:
+        """Whenever someone wins / player leaves it quits to main menu."""
         pyautogui.hotkey("esc")
-        if ImageRecognition.find_and_click(IMAGE_PATHS["return_to_main_menu_button"], clicks=2):
-            logging.info("Restart game.")
-        
-        detect_stage()
+        RoomSetup._click_and_log(img_path, log_message="Restart game.", clicks=2)
--- main/main.py
+++ main/main.py
@@ -1,12 +1,12 @@
 from config import IMAGE_PATHS
-import logging  # (Built-in) Used to print classy messages
+from logging import info  # (Built-in) Used to print classy messages
 from functions import (
     ImageRecognition,
     Backend,
-    Stage,
+    RoomSetup,
 )
 
-# Function used to detect the current stage of the game
+
 def detect_stage() -> int:
     """
     Takes a look at your screen using is_on_screen() and figures out what stage you are on.
@@ -25,17 +25,17 @@ def detect_stage() -> int:
             return 4
         if ImageRecognition.is_on_screen(IMAGE_PATHS["close_invites_button"]):
             return 5
-        if ImageRecognition.is_on_screen(IMAGE_PATHS["player_left"]):
-            logging.info("Player Left")
-            return 6
         if ImageRecognition.is_on_screen(IMAGE_PATHS["white_wins"]):
-            logging.info("White Won")
+            info("White Won")
             return 6
         if ImageRecognition.is_on_screen(IMAGE_PATHS["black_wins"]):
-            logging.info("Black Won")
+            info("Black Won")
+            return 6
+        if ImageRecognition.is_on_screen(IMAGE_PATHS["player_left"]):
+            info("Player Left")
             return 6
         if ImageRecognition.is_on_screen(IMAGE_PATHS["pending_connection_failure"]):
-            logging.info("Connection Failed")
+            info("Connection Failed")
             return 7
 
 # The main flow of the program is here
@@ -44,35 +44,43 @@ def create_lobby(game_mode_flag: bool) -> None:
     """
     Creates a lobby by looping through a few functions -- Based on game_mode_flag and define_stage function.
     :param game_mode_flag: If True, sets up a shiny chess game. Otherwise, sets up a normal game.
-    :return: None
     """
     WIN_LOSS_SCENARIO: int = 6
      
-    logging.info("Detecting stage...")
+    info("Detecting stage...")
     current_stage: int = detect_stage() # Get current stage
 
-    functions: list = [
-        Stage.find_host_button,
-        Stage.set_room_name,
-        Stage.set_description,
-        Stage.set_game_mode,
-        Stage.create_room,
-        Stage.close_invites,
-        ]
+    FUNCTIONS: tuple = (
+        RoomSetup.find_host_button,
+        RoomSetup.set_room_name,
+        RoomSetup.set_description,
+        RoomSetup.set_game_mode,
+        RoomSetup.create_room,
+        RoomSetup.close_invites,
+    )
+    
+    IMAGES: tuple = (
+        IMAGE_PATHS["host_text_in_main_menu"],
+        IMAGE_PATHS["room_name_text_box"],
+        IMAGE_PATHS["description_text_box"],
+        IMAGE_PATHS["shiny_game_mode_checkbox"],
+        IMAGE_PATHS["create_room_button"],
+        IMAGE_PATHS["close_invites_button"],
+    )
 
-    logging.info("Creating lobby...")
-    # Looping through stages
+    info("Creating lobby...")
+    # Looping through room creating stages
     if current_stage < WIN_LOSS_SCENARIO:
-        for func in functions[current_stage:]:
-                func(game_mode_flag)
+        for func, image in zip(FUNCTIONS[current_stage:], IMAGES[current_stage:]):
+            func(image, game_mode_flag) 
 
     # Game Over
     else:
-        Stage.game_ended(game_mode_flag)
+        RoomSetup.game_ended(IMAGE_PATHS["return_to_main_menu_button"], game_mode_flag)
 
 
 def main() -> None:
-    logging.info("Macro ON")
+    info("Macro ON")
     USER_INPUT: str = Backend.user_input()  # So the program won't ask for another input when looping once
     while True:  # This program doesn't have a concrete way to stop
         create_lobby(USER_INPUT)
